966e05ffb0d12c86b2dfeada1fd52a7628d0909c
Hieu Lai <ldmhieudev@gmail.com>
2024-04-15 16:14:04 +0700

    chua biet sua gi

--- src/dao/impl/Main.java
+++ src/dao/impl/Main.java
@@ -6,7 +6,8 @@ import entityJPA.TaiKhoan;
 public class Main {
 	public static void main(String[] args) {
 		TaiKhoanDao taiKhoanDao = new TaiKhoanImpl(TaiKhoan.class);
-		TaiKhoan taiKhoan = new TaiKhoan("nguyentanloc1108", "NTL@1108", "example@gmail.com");
+		TaiKhoan taiKhoan = new TaiKhoan("hieulai","123123","hieulaidev");
+
 		taiKhoanDao.save(taiKhoan);
 		System.out.println(taiKhoanDao.xacThucNguoiDung("example@gmail.com", "NTL@1108"));
 		//taiKhoanDao.phanQuyen("example@gmail.com");
