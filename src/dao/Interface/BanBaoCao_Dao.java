2def4c9c5b79c31bff5ef5d9d85d67ae3be04dee
colnat412 <dieglevel@gmail.com>
2024-04-20 14:05:04 +0700

    option Generic

--- src/dao/Interface/BanBaoCao_Dao.java
+++ src/dao/Interface/BanBaoCao_Dao.java
@@ -5,6 +5,5 @@ import entityJPA.BanBaoCao;
 import java.rmi.Remote;
 
 public interface BanBaoCao_Dao extends GenericDao<BanBaoCao>, Remote {
-    @Override
-    public boolean delete(Object id);
+
 }
