cca2ae904d574e69f4c2ead700e4efe54f20a772
colnat412 <dieglevel@gmail.com>
2024-04-22 11:27:08 +0700

    CHiTiet+BaoCao

--- src/dao/impl/BanBaoCao_Impl.java
+++ src/dao/impl/BanBaoCao_Impl.java
@@ -1,7 +1,8 @@
 package dao.impl;
 
 import dao.Interface.BanBaoCao_Dao;
-import entity.BanBaoCao;
+import entityJPA.BanBaoCao;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -12,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BanBaoCao_Impl extends UnicastRemoteObject implements BanBaoCao_Dao {
-	private EntityManagerFactory emf;
+	private final EntityManagerFactory emf;
 	public BanBaoCao_Impl(EntityManagerFactory emf) throws RemoteException {
 		super();
 		this.emf = emf;
@@ -28,25 +29,14 @@ public class BanBaoCao_Impl extends UnicastRemoteObject implements BanBaoCao_Dao
 		return generic.save(bbc);
 	}
 
+	public int getMaBBCTheoTen(String tenBBC) throws RemoteException {
+		GenericImpl<BanBaoCao> generic = new GenericImpl<>(BanBaoCao.class, emf);
+		List<BanBaoCao> list = generic.findByProperty("tenBanBaoCao", tenBBC);
 
+		if (!list.isEmpty()) {
+			return list.get(0).getMaBanBaoCao();
+		}
 
-//	public ArrayList<BanBaoCao> getALLBanBaoCao() throws RemoteException {
-//		return (ArrayList<BanBaoCao>) generic.findAll();
-//	}
-//
-//	public boolean taoBanBaoCao(BanBaoCao bbc) throws RemoteException {
-//		return generic.save(bbc);
-//	}
-//
-//	public int getMaBBCTheoTen(String tenBBC) throws RemoteException {
-//		List<BanBaoCao> list = generic.findByProperty("tenBanBaoCao", tenBBC);
-//
-//		if (!list.isEmpty()) {
-//			return list.get(0).getMaBanBaoCao();
-//		}
-//
-//		return -1;
-//	}
-
-
+		return -1;
+	}
 }
