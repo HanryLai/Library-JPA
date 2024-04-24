05ba5a83c498d1b13c98320006b3b6ba31fb0e3e
colnat412 <nguyentanloc1108@gmail.com>
2024-04-24 21:10:34 +0700

    Done GUI

--- src/menuGui/MenuEvent.java
+++ src/menuGui/MenuEvent.java
@@ -4,5 +4,5 @@ import java.rmi.RemoteException;
 
 public interface MenuEvent {
 
-    public void selected(int index, int subIndex) throws RemoteException;
+    public void selected(int index, int subIndex) throws Exception;
 }
