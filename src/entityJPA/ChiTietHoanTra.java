22bfcd9f7e9b4a150d04b701dbcba0d377780f8c
colnat412 <dieglevel@gmail.com>
2024-04-25 20:32:11 +0700

    change struct

--- src/entityJPA/ChiTietHoanTra.java
+++ src/entityJPA/ChiTietHoanTra.java
@@ -1,13 +1,10 @@
 package entityJPA;
 
-import entityJPA.otherID.ChiTietDoiHangID;
+import entityJPA.otherID.ChiTietHoanTraID;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.io.Serializable;
 
